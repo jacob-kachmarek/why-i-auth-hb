2899c1d67a8c26832fded718a1ff2d163b20e726
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-25 14:37:58 -0800

    added sign up form event listener, working on the second

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
+redirectIfLoggedIn();
 // Wire up sign in and sign up forms to supabase
-// Redirect to /other-page on successful auth
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+    // Redirect to /other-page on successful auth
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = await signInUser(signInEmail, signInPassword);
+    if (user)
+});
+
 // Redirect to /other-page when page loads if user is authenticated
